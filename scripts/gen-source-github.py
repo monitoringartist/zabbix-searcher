b6a910fcfb697936a695dc49ebb0823082f1d27f
Jan Garaj <jan.garaj@gmail.com>
2015-09-27 11:43:55 +0100

    github source update

--- scripts/gen-source-github.py
+++ scripts/gen-source-github.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import os, sys, requests, json, base64
+from collections import OrderedDict
 
 def github_query(url):
      
-    headers = {'Authorization': 'token 1dc33912a4668751497d77bc536fe6f87d577965'}
+    headers = {'Authorization': 'token ???'}
             
     s = requests.get(url, headers=headers)
     if s.status_code != 200:
@@ -17,6 +18,7 @@ def github_query(url):
 s = github_query('https://api.github.com/repos/zabbix/zabbix-community-repos/git/trees/master')
 s = json.loads(s)
 arr = {}
+top = {}
 result = 1
 for ofile in s['tree']:
     try: 
@@ -24,10 +26,17 @@ for ofile in s['tree']:
     except:
         continue   
     #print str(result) + '. ' + ofile['path'] + ' - https://github.com/zabbix/zabbix-community-repos/tree/master/' + ofile['path']
-    arr[ofile['path']] = {
-      'url': 'https://github.com/zabbix/zabbix-community-repos/tree/master/' + ofile['path'],
-      'keywords': ofile['path'].lower().replace('(','').replace(')','').split('-'),
-      'name': ofile['path'][0:ofile['path'].index('(')].replace('-',' ') + ofile['path'][ofile['path'].index('('):] 
-    }
-#print arr    
-print json.dumps(arr)         
+    if 'monitoringartist' in ofile['path']:
+        top[ofile['path']] = {
+          'url': 'https://github.com/zabbix/zabbix-community-repos/tree/master/' + ofile['path'],
+          'keywords': ofile['path'].lower().replace('(','').replace(')','').split('-'),
+          'name': ofile['path'][0:ofile['path'].index('(')].replace('-',' ') + ofile['path'][ofile['path'].index('('):] 
+        }    
+    else:  
+        arr[ofile['path']] = {
+          'url': 'https://github.com/zabbix/zabbix-community-repos/tree/master/' + ofile['path'],
+          'keywords': ofile['path'].lower().replace('(','').replace(')','').split('-'),
+          'name': ofile['path'][0:ofile['path'].index('(')].replace('-',' ').replace('  ',' ') + ofile['path'][ofile['path'].index('('):] 
+        }
+#print arr     
+print json.dumps(OrderedDict(sorted(top.items(), key=lambda t: t[0])+sorted(arr.items(), key=lambda t: t[0])))
