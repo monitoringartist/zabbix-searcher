d0ee286b922d0939a41f85b08c31f6ccf3981d5a
jangaraj <jan.garaj@gmail.com>
2015-10-01 23:40:06 +0100

    automation

--- scripts/regenerate-sources.sh
+++ scripts/regenerate-sources.sh
@@ -1,3 +1,5 @@
 python gen-source-github.py > ../sources/github-community-repos.json
 python gen-source-share.py > ../sources/share-zabbix.json 
 python gen-source-wiki.py > ../sources/zabbix-wiki.json
+DATE=`date +"%Y-%m-%d %H:%M:%S"`
+sed -i -e "s/Updated on .*\./Updated on $DATE./g" ../index.html
