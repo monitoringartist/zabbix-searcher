9dbd66a599ac9778a4af2c6ef29da0a473428e7a
jangaraj <jan.garaj@gmail.com>
2015-10-01 23:13:20 +0100

    automation

--- scripts/regenerate-sources.sh
+++ scripts/regenerate-sources.sh
@@ -1,6 +1,3 @@
 python gen-source-github.py > ../sources/github-community-repos.json
 python gen-source-share.py > ../sources/share-zabbix.json 
 python gen-source-wiki.py > ../sources/zabbix-wiki.json
-DATE=`date +%Y-%m-%d`
-git commit -am "Source update $DATE"
-#git push origin gh-pages 
