f291aff04413f68fd83272534644801cfbb44cf2
Jan Garaj <jan.garaj@gmail.com>
2017-02-07 21:02:31 +0000

    Travis automation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -52,8 +52,6 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 cd ..
-pwd
-ls -lah
 git add sources
 git commit -m "CI build deployment to GitHub Pages: ${SHA}"
 
@@ -62,7 +60,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../scripts/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in scripts/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
